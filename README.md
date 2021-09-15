2f208681ffd2b0a6168bbebe98170741ffbef198
sanjbabu <sanjay.babu@gov.bc.ca>
2021-09-15 14:24:55 -0700

    clean up

--- README.md
+++ README.md
@@ -15,46 +15,19 @@ Windows Subsystem for Linux is a compatibility layer for running Linux binary ex
 5. Run on cmd "wsl --set-default-version 2" (Set your distribution version to WSL 2)
 
 ```
-
-## 2. Install python, pip, dependencies and vault ##
-
-Open Windows terminal and choose Ubuntu-20.04 and run the following commands
-
-```
-1. #Update repo
-sudo apt update
-
-2. #Install python3
-sudo apt install python3
-
-3. #Install python package manager pip
-sudo apt install python3-pip
-
-4. #Install python library to read KeePass database
-pip install pykeepass
-
-5. #Install python vault api client
-pip install hvac
-
-6. #Install json processor to process json response received from vault login with oidc and obtain vault token
-sudo apt install jq
-
-7. #Install vault (https://learn.hashicorp.com/tutorials/vault/getting-started-install)
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install vault
-```
-
 ## 3. Replace environment variables in env-dev with appropriate values ##
 
+## Obtain vault token from UI by going to https://vault-iit-dev.apps.silver.devops.gov.bc.ca/ and click on profile icon on the top right hand corner and Copy token
+## Sample KeePass file password: 12345678
 
 ```
-export VAULT_ADDR=https://vault-iit-dev.apps.silver.devops.gov.bc.ca/
-export VAULT_TOKEN=$(vault login -method=oidc -format json | jq -r '.auth.client_token')
-export KEEPASS_PATH=<path>
-export KEEPASS_PWD=<pwd>
-export MOUNT_POINT=<path of key value v2 secrets engine>
-export SECRETS_PATH=<path to secrets>
+VAULT_ADDR=https://vault-iit-dev.apps.silver.devops.gov.bc.ca/
+VAULT_TOKEN=
+KEEPASS_PATH=<path>
+KEEPASS_PWD=<pwd>
+MOUNT_POINT=<path of key value v2 secrets engine>
+SECRETS_PATH=<path to secrets>
+
 ```
 
 ## 4. Execution ##
@@ -68,7 +41,7 @@ Run the following commands in the terminal:
 source env-dev
 
 #run the script
-python3 py-keepass-vault.py
+docker run --rm --name container1 --env-file env-dev -v "$(pwd):/home" appdev:test bash -c "python3 py-keepass-vault.py"
 
 #list the data you loaded
 vault kv list -format yaml user/andreas.wilson@gov.bc.ca/test-load
--- py-keepass-vault.py
+++ py-keepass-vault.py
@@ -3,8 +3,7 @@ import hvac
 import os
 import sys
 
-# load database
-#kp = PyKeePass(os.getenv('KEEPASS_PATH','test'), password=os.getenv('KEEPASS_PWD','test'))
+# reading environment variables
 ENV_KEEPASS_PATH = os.environ['KEEPASS_PATH']
 ENV_KEEPASS_PWD = os.environ['KEEPASS_PWD']
 ENV_VAULT_ADDR = os.environ['VAULT_ADDR']
@@ -12,73 +11,20 @@ ENV_VAULT_TOKEN = os.environ['VAULT_TOKEN']
 ENV_MOUNT_POINT = os.environ['MOUNT_POINT']
 ENV_SECRETS_PATH = os.environ['SECRETS_PATH']
 
-
-
-
-#ENV_KEEPASS_PATH = 'sample.kdbx'
-ENV_KEEPASS_PWD = '12345678'
-# ENV_VAULT_ADDR = 'https://vault-iit-dev.apps.silver.devops.gov.bc.ca/'
-#ENV_VAULT_TOKEN = 's.OCiaMT20TJap1z7rYOdyffpW'
-# ENV_MOUNT_POINT = 'user'
-# ENV_SECRETS_PATH = 'sanjay.babu@gov.bc.ca/test-load7'
-
-
-
-
-
+# load database
 kp = PyKeePass(ENV_KEEPASS_PATH, password=ENV_KEEPASS_PWD)
-
-#kp = PyKeePass(os.environ['KEEPASS_PATH'], password=os.environ['KEEPASS_PWD'])
 
 # read KeePass entries
 fullList = kp.entries
 
 # loading vault url and token
-# client = hvac.Client(url=os.environ['VAULT_ADDR'])
-# client.token = os.environ['VAULT_TOKEN']
-
-
-
 client = hvac.Client(ENV_VAULT_ADDR)
 client.token = ENV_VAULT_TOKEN
 
-
-# writing log
-#old_stdout = sys.stdout
-
-#log_file = open("message.log","w")
-
-#sys.stdout = log_file
-
-print ("this will be written to message.log")
-
-#print (os.environ['PWD'])
-
-
-
-
 # pushing KeePass entries to vault
 # create a new v2 secrets engine using vault ui and set it as mount point
-# for entry in fullList:
-#     try:
-#         create_response = client.secrets.kv.v2.create_or_update_secret(mount_point=os.environ['MOUNT_POINT'], path=os.environ['SECRETS_PATH']+'/'+entry.title, secret=dict(title=entry.title, username=entry.username, password=entry.password, notes=entry.notes))
-#     except Exception as error:
-#         print (error.args)
-
-
-# for entry in fullList:
-#     try:
-#         create_response = client.secrets.kv.v2.create_or_update_secret(mount_point=os.environ['MOUNT_POINT'], path=os.environ['SECRETS_PATH']+'/'+entry.title, secret=dict(title=entry.title, username=entry.username, password=entry.password, notes=entry.notes))
-#     except Exception as error:
-#         print (error.args)
-
 for entry in fullList:
     try:
-        #create_response = client.secrets.kv.v2.create_or_update_secret('user', 'sanjay.babu@gov.bc.ca/test-load3'+'/'+entry.title, secret=dict(title=entry.title, username=entry.username, password=entry.password, notes=entry.notes))
         create_response = client.secrets.kv.v2.create_or_update_secret(mount_point=ENV_MOUNT_POINT, path=ENV_SECRETS_PATH+'/'+entry.title, secret=dict(title=entry.title, username=entry.username, password=entry.password, notes=entry.notes))
-   
     except Exception as error:
-        print (error.args)     
-
-#sys.stdout = old_stdout
-#log_file.close()
+        print (error.args)
