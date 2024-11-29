21b6b09ba9a28591769c97cd5668930ce7b7bf88
van der Hooft, David WLRS:EX <david.vanderhooft@gov.bc.ca>
2024-11-29 14:53:36 -0800

    Change automated upload semantics.
     - remove notes from metadata
     - add expiry to metadata
     - refactor entry_path to variable
     - append uuid to path for uniqueness
     - do not split on spaces in groups in entry_path
     - log entry_path before uploading to Vault

--- scripts/py-keepass-vault.py
+++ scripts/py-keepass-vault.py
@@ -25,20 +25,26 @@ client.token = ENV_VAULT_TOKEN
 # create a new v2 secrets engine using vault ui and set it as mount point
 for entry in fullList:
     try:
-        create_response = client.secrets.kv.v2.create_or_update_secret(mount_point=ENV_MOUNT_POINT, path=ENV_SECRETS_PATH+'/'+ (str(entry.group).split())[1].split('"')[1]+ '/' +entry.title.replace("/","_"), secret=dict(username=entry.username, password=entry.password))
+        entry_path = ENV_SECRETS_PATH+'/'+ str(entry.group).split('"')[1]+ '/' +entry.title.replace("/","_")+'-'+entry.uuid.hex
+        print(entry_path)
+
+        create_response = client.secrets.kv.v2.create_or_update_secret(
+                mount_point=ENV_MOUNT_POINT,
+                path=entry_path,
+                secret=dict(username=entry.username, password=entry.password))
+
         custom_metadata={
             "keepass_title": entry.title,
             "keepass_filename": kfile,
             "keepass_uuid": entry.uuid.hex,
             "keepass_url": entry.url if entry.url else None,
-            "keepass_notes": entry.notes if entry.notes else None
+            "keepass_expiry": str(entry.expiry_time) if entry.expires else None,
         }
         custom_metadata = {key: value for key, value in custom_metadata.items() if value is not None}
 
         client.secrets.kv.v2.update_metadata(
-            path=ENV_SECRETS_PATH+'/'+ (str(entry.group).split())[1].split('"')[1] + '/' +entry.title.replace("/","_"),
+            path=entry_path,
             custom_metadata=custom_metadata
         )
-        print((str(entry.group).split())[1].split('"')[1])
     except Exception as error:
         print (error.args)
