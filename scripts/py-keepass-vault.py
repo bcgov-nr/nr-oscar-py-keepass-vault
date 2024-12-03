0671e31ca49efd8f9d7e5c62567552da9f5599b3
David van der Hooft <163213729+dvndrh@users.noreply.github.com>
2024-12-03 11:56:11 -0800

    Feature/refine automated upload semantics (#4)
    * Change automated upload semantics.
    
     - remove notes from metadata
     - add expiry to metadata
     - refactor entry_path to variable
     - append uuid to path for uniqueness
     - do not split on spaces in groups in entry_path
     - log entry_path before uploading to Vault
    
    * Prepend Vault URL to KeePass notes
    
     - avoid '//' in entry_path for root_group
    
    * Add mount_point to update_metadata
    
     - additional output to clarify order of operations

--- scripts/py-keepass-vault.py
+++ scripts/py-keepass-vault.py
@@ -1,6 +1,7 @@
 from pykeepass import PyKeePass
 import hvac
 import os
+import urllib.parse
 
 # reading environment variables
 ENV_KEEPASS_PATH = os.environ['KEEPASS_PATH']
@@ -25,20 +26,37 @@ client.token = ENV_VAULT_TOKEN
 # create a new v2 secrets engine using vault ui and set it as mount point
 for entry in fullList:
     try:
-        create_response = client.secrets.kv.v2.create_or_update_secret(mount_point=ENV_MOUNT_POINT, path=ENV_SECRETS_PATH+'/'+ (str(entry.group).split())[1].split('"')[1]+ '/' +entry.title.replace("/","_"), secret=dict(username=entry.username, password=entry.password))
+        coerced_entry_group = ('' if entry.group.is_root_group else '/'+str(entry.group).split('"')[1])
+        entry_path = ENV_SECRETS_PATH+coerced_entry_group+'/'+entry.title.replace("/","_")+'-'+entry.uuid.hex
+        print("Creating" + entry_path)
+
+        create_response = client.secrets.kv.v2.create_or_update_secret(
+                mount_point=ENV_MOUNT_POINT,
+                path=entry_path,
+                secret=dict(username=entry.username, password=entry.password))
+
+        print("Updating metadata.")
         custom_metadata={
             "keepass_title": entry.title,
             "keepass_filename": kfile,
             "keepass_uuid": entry.uuid.hex,
             "keepass_url": entry.url if entry.url else None,
-            "keepass_notes": entry.notes if entry.notes else None
+            "keepass_expiry": str(entry.expiry_time) if entry.expires else None,
         }
         custom_metadata = {key: value for key, value in custom_metadata.items() if value is not None}
 
         client.secrets.kv.v2.update_metadata(
-            path=ENV_SECRETS_PATH+'/'+ (str(entry.group).split())[1].split('"')[1] + '/' +entry.title.replace("/","_"),
+            mount_point=ENV_MOUNT_POINT,
+            path=entry_path,
             custom_metadata=custom_metadata
-        )
-        print((str(entry.group).split())[1].split('"')[1])
+        ) 
+
+        print("Writing URL to KeePass file.")
+        vault_url = ENV_VAULT_ADDR+"/ui/vault/secrets/"+ENV_MOUNT_POINT+"/kv/"+urllib.parse.quote(entry_path, safe='')+"/details"
+        entry.notes = "Vault URL: "+vault_url+"\n\n"+(entry.notes if entry.notes else '')
+
+
     except Exception as error:
         print (error.args)
+
+kp.save()
